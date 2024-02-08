3214afa97e95cc432108533cd28ceb2cd9e13cc5
xDMPx <57721731+xDMPx@users.noreply.github.com>
2024-02-08 17:31:33 +0100

    Add basic notification and background location permission request

--- app/src/main/java/com/xdmpx/routesp/MainActivity.kt
+++ app/src/main/java/com/xdmpx/routesp/MainActivity.kt
@@ -6,36 +6,29 @@ import android.content.DialogInterface
 import android.content.DialogInterface.OnDismissListener
 import android.content.Intent
 import android.content.pm.PackageManager
-import android.graphics.drawable.BitmapDrawable
-import android.icu.text.CaseMap.Title
-import android.location.Location
 import android.location.LocationManager
 import android.net.Uri
+import android.os.Build
 import android.os.Bundle
 import android.provider.Settings
 import android.util.Log
 import android.view.View
-import android.view.Window
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
-import androidx.core.content.res.ResourcesCompat
-import androidx.preference.PreferenceManager
-import com.google.android.gms.location.LocationServices
-import com.google.android.gms.location.Priority
-import org.osmdroid.config.Configuration
-import org.osmdroid.tileprovider.tilesource.TileSourceFactory
-import org.osmdroid.util.GeoPoint
-import org.osmdroid.views.MapController
-import org.osmdroid.views.MapView
-import org.osmdroid.views.overlay.compass.CompassOverlay
-import org.osmdroid.views.overlay.compass.InternalCompassOrientationProvider
-import org.osmdroid.views.overlay.gestures.RotationGestureOverlay
-import org.osmdroid.views.overlay.mylocation.GpsMyLocationProvider
-import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay
 
 class MainActivity : AppCompatActivity() {
     private val DEBUG_TAG = "MainActivity"
+
+    private var requestedNotificationPermission = false
+    private val requestNotificationPermissionLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestPermission()
+    ) { isGranted: Boolean ->
+        requestedNotificationPermission = true
+        requestPermissions(PermissionType.LOCATION)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -45,13 +38,42 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun goToMapActivity(view: View) {
-        when (requestLocation()) {
-            true -> {
-                val intent = Intent(this, MapActivity::class.java)
-                startActivity(intent)
+        if (requestPermissions()) {
+            val intent = Intent(this, MapActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+    enum class PermissionType {
+        NOTIFICATION, LOCATION,
+    }
+
+    private fun requestPermissions(permissionType: PermissionType = PermissionType.NOTIFICATION): Boolean {
+        return when (permissionType) {
+            PermissionType.NOTIFICATION -> {
+                if (!requestedNotificationPermission && Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                    return requestNotificationPermission()
+                } else {
+                    requestPermissions(PermissionType.LOCATION)
+                }
             }
 
-            false -> {}
+            PermissionType.LOCATION -> {
+                requestLocation()
+            }
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.TIRAMISU)
+    private fun requestNotificationPermission(): Boolean {
+        return if (ContextCompat.checkSelfPermission(
+                this, Manifest.permission.POST_NOTIFICATIONS
+            ) == PackageManager.PERMISSION_DENIED
+        ) {
+            requestNotificationPermissionLauncher.launch(Manifest.permission.POST_NOTIFICATIONS)
+            false
+        } else {
+            true
         }
     }
 
@@ -91,6 +113,24 @@ class MainActivity : AppCompatActivity() {
                     ).apply { startActivity(this) }
                 }
                 Log.e(DEBUG_TAG, "ACCESS_FINE_LOCATION")
+            }
+
+            ContextCompat.checkSelfPermission(
+                this, Manifest.permission.ACCESS_BACKGROUND_LOCATION
+            ) == PackageManager.PERMISSION_DENIED -> {
+                showAlertDialog(this@MainActivity,
+                    getString(R.string.please_grant_required_permission),
+                    getString(R.string.location_permission),
+                    getString(R.string.ok),
+                    {}) { dialog, _ ->
+                    dialog?.dismiss()
+                    Intent(
+                        Settings.ACTION_APPLICATION_DETAILS_SETTINGS, Uri.parse(
+                            "package:" + BuildConfig.APPLICATION_ID
+                        )
+                    ).apply { startActivity(this) }
+                }
+                Log.e(DEBUG_TAG, "ACCESS_BACKGROUND_LOCATION")
             }
 
             !isLocationEnabled() -> {
