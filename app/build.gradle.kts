e6bcb9b7fc893a16b4a4d20d518ac4917fc31ff4
xDMPx <57721731+xDMPx@users.noreply.github.com>
2024-08-31 19:54:59 +0200

    Upgrade AGP dependency from 8.5.1 to 8.5.2. Bump dependencies.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,7 +55,7 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
 
-    implementation("org.osmdroid:osmdroid-android:6.1.18")
+    implementation("org.osmdroid:osmdroid-android:6.1.20")
     implementation("androidx.preference:preference-ktx:1.2.1")
 
     val roomVersion = "2.6.1"
